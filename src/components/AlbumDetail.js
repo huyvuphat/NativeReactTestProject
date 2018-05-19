fcbcb53185087dd86b57a0b90147e56b6793af7a
Huy Vu <hpvu@uno.edu>
2018-05-19 16:49:22 -0500

    Adding images to the app

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,18 +1,68 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+     thumnailStyle,
+     headerContentStyle,
+     thumnailContainerStyle,
+     headerTextStyle,
+     imageStyle
+   } = styles;
   return (
     <Card>
     <CardSection>
-      <Text>{props.album.title}</Text>
-     </CardSection>
+      <View style={thumnailContainerStyle}>
+        <Image
+          style={thumnailStyle}
+          source={{ uri: thumbnail_image }}
+        />
+      </View>
 
+      <View style={headerContentStyle}>
+        <Text style={headerTextStyle}>{title}</Text>
+        <Text>{artist}</Text>
+      </View>
+     </CardSection>
+     <CardSection>
+        <Image
+        style={imageStyle}
+        source={{ uri: image }}
+        />
+     </CardSection>
 
     </Card>
   );
 };
 
+
+const styles = {
+  headerContentStyle: { //style for title and artist
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumnailStyle: {
+    height: 50,
+    width: 50
+  },
+
+  thumnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+};
 export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -21,12 +21,13 @@ renderAlbums() {
   );
 }
 
+
 render() {
  console.log(this.state);
       return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-       </View>
+       </ScrollView>
       );
   }
 
